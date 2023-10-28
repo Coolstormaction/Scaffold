c0eb5fccf73897b2ff9d31df58e3cba1ffd0f32d
Coolstormaction <halderhena05@gmail.com>
2023-10-28 20:24:57 +0530

    added batch files for global environment commands

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import sys 
+import sys
+from errors import * 
+
+projectName, projectType = sys.argv[1], sys.argv[2]
 
 if len(sys.argv) < 4:
-    ...
+    raise InsufficientArguments('Insufficient Arguments Provided, (scaffold <path> <projectType>)')
+
+print(projectName, projectType)
